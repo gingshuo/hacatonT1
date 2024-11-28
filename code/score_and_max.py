2052133d705aa9d74633f7d3ef896c08f62a8683
gingshuo <148972248+gingshuo@users.noreply.github.com>
2024-11-28 15:23:51 +0300

    Update score_and_max.py
    repair the problem can't find output folder

--- code/score_and_max.py
+++ code/score_and_max.py
@@ -1,26 +1,22 @@
 import pandas as pd
 from datetime import datetime
 import os
+
 # 动态设置工作目录为脚本所在目录
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
-
 from scoring import score_record  # 从 scoring.py 引用打分函数
 from load_and_preprocess import load_and_preprocess
-
-
 
 def extract_golden_records(df):
     """
     提取每个ID组的“黄金记录”，并按照score降序排列。
     """
-
-
     # 为数据打分
     df['score'] = df.apply(score_record, axis=1)
 
-    # 删除 score 低于 30 的行
+    # 删除 score 低于 55 的行
     df_filtered = df[df['score'] >= 55]
 
     # 按 'client_fio_full' 分组，并选择每组中 score 最大的记录
@@ -36,6 +32,12 @@ def save_to_csv(df, output_path):
     """
     将排序后的数据保存到CSV文件。
     """
+    # 检查并创建输出文件夹
+    output_dir = os.path.dirname(output_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    # 保存数据到 CSV 文件
     df.to_csv(output_path, index=False, date_format='%Y-%m-%d %H:%M:%S')
 
 if __name__ == "__main__":
